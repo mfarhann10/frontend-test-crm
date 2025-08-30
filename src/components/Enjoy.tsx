4a3ee427f459ca0ba485f37042516eec765b6d4e
mfarhann10 <mfarhann1009@gmail.com>
2025-08-30 23:00:38 +0700

    responsive to enjoy

--- src/components/Enjoy.tsx
+++ src/components/Enjoy.tsx
@@ -3,26 +3,35 @@ import { Button } from "./ui/button";
 export const Enjoy = () => {
   return (
     <section id="enjoy" className="py-16">
-      <div className="flex flex-row gap-x-8">
+      <div className="flex flex-col md:flex-row gap-8">
         {/* Text Column */}
-        <div className="flex flex-col space-y-4">
-          <h1 className="font-poppins font-semibold text-[40px] leading-[44px] tracking-[-0.023em] text-[#0A0A0A] w-[412px]">
+        <div className="flex flex-col space-y-4 md:w-auto">
+          <h1 className="font-poppins font-semibold text-[32px] md:text-[40px] leading-[36px] md:leading-[44px] tracking-[-0.023em] text-[#0A0A0A]">
             Various <span className="text-[#106A64]">facilities </span>
             that you can enjoy
           </h1>
-          <p className="w-[348px] font-poppins text-xl tracking-normal text-[#9E9E9E]">
+          <p className="font-poppins text-lg md:text-xl tracking-normal text-[#9E9E9E]">
             We bring you together with your dream holiday
           </p>
-          <Button className="w-[191px] h-[52px]">Explore</Button>
+          <Button className="w-[150px] md:w-[191px] h-[48px] md:h-[52px]">
+            Explore
+          </Button>
         </div>
 
         {/* Images */}
-        <div className="flex flex-row gap-x-4">
-          <img src="enjoy.png" alt="Enjoy" className="w-[319px] h-[319px] object-cover " />
-          <img src="enjoy2.png" alt="Enjoy" className="w-[319px] h-[319px] object-cover " />
+        <div className="flex flex-row gap-4 mt-4 md:mt-0">
+          <img
+            src="enjoy.png"
+            alt="Enjoy"
+            className="w-[157px] h-[157px] md:w-[319px] md:h-[319px] object-cover "
+          />
+          <img
+            src="enjoy2.png"
+            alt="Enjoy"
+            className="w-[157px] h-[157px] md:w-[319px] md:h-[319px] object-cover "
+          />
         </div>
       </div>
     </section>
   );
 };
-    
